c6a8e59908c3584174a557a9979c255ec889a7dc
qiraxyz <ariqaziz74@gmail.com>
2022-12-27 18:18:18 +0700

    class api

---
+++ app/helpers/ApiFormatter.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\helpers;
+
+
+class ApiFormatter{
+    protected static $response = [
+        'date' => null,
+        'message' => null,
+        'data' => null,
+    ];
+
+    public static function createApi($date = null, $message = null, $data = null) {
+        self::$response['date'] = $date;
+        self::$response['pesan'] = $message;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['date']);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB as data;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        data::table('pesans')->insert([
+            'usernames' => 'ghrimxi',
+            'pesan' => 'bro kenalin',
+            'updated_at' => now(),
+            'created_at' => now(),
+        ]);
     }
 }
